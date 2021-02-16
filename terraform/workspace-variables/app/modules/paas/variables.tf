9691129ec2584efa881761217361d70c7a2765b6
Steven <steven.woods@digital.education.gov.uk>
2021-02-16 20:14:32 +0000

    WIP: add terraform workspace variables

---
+++ terraform/workspace-variables/app/modules/paas/variables.tf
@@ -0,0 +1,52 @@
+variable environment {
+}
+
+variable app_docker_image {
+}
+
+variable app_env_values {
+}
+
+variable app_start_timeout {
+  default = 300
+}
+
+variable app_stopped {
+  default = false
+}
+
+variable service_name {
+}
+variable space_name {
+}
+
+variable web_app_deployment_strategy {
+}
+
+variable web_app_instances {
+  default = 1
+}
+
+variable web_app_memory {
+  default = 512
+}
+
+variable web_app_start_command {
+}
+
+locals {
+
+  app_env_domain  = {
+    "DOMAIN" = "dfe-meeting-timer-${var.environment}.london.cloudapps.digital"
+    "GOVUK_APP_DOMAIN" = "dfe-meeting-timer-${var.environment}.london.cloudapps.digital"
+    "GOVUK_WEBSITE_ROOT" = "dfe-meeting-timer-${var.environment}.london.cloudapps.digital"
+  }
+  app_environment = merge(
+    local.app_env_domain,
+    var.app_env_values #Because of merge order, if present, the value of DOMAIN in .tfvars will overwrite app_env_domain
+  )
+  app_service_bindings = concat(
+    local.app_cloudfoundry_service_instances,
+  )
+  web_app_name             = "${var.service_name}-${var.environment}"
+}
