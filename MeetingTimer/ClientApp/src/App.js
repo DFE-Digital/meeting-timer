5d7a54f09f5862e34983acde96285cbb243d4952
9080tom <thomas.thickett@isotoma.com>
2021-03-05 21:31:28 +0000

    using-refs-to-initialise-components-with-govuk-frontend

--- MeetingTimer/ClientApp/src/App.js
+++ MeetingTimer/ClientApp/src/App.js
@@ -1,17 +1,15 @@
 import React, { useLayoutEffect } from 'react';
 import { Route } from 'react-router';
-import { useLocation } from 'react-router-dom';
 import { initAll } from 'govuk-frontend';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 
 const App = () => {
-  const { pathname } = useLocation();
 
   useLayoutEffect(() => {
     initAll();
-  }, [pathname]);
+  },[]);
 
   return (
     <Layout>
--- MeetingTimer/ClientApp/src/pages/About.js
+++ MeetingTimer/ClientApp/src/pages/About.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AccordionItem from '../components/AccordionItem';
+import useGovUKFrontend from '../utils/initialiseComponent'
 
-const About = () => (
-  <div className="govuk-width-container">
-    <Link to="/" className="govuk-back-link">
-      Back
-    </Link>
-    <h1>About</h1>
-    <div
-      className="govuk-accordion"
-      data-module="govuk-accordion"
-      id="accordion-default"
-    >
-      <AccordionItem title="Hello" content="World" />
-      <AccordionItem title="Hello" content="World" />
-      <AccordionItem title="Hello" content="World" />
+const About = () => {
+  const inputEl = useGovUKFrontend();
+
+  return (
+    <div className="govuk-width-container" ref={inputEl}>
+      <Link to="/" className="govuk-back-link">
+        Back
+      </Link>
+      <h1>About</h1>
+      <div
+        className="govuk-accordion"
+        data-module="govuk-accordion"
+        id="accordion-default"
+      >
+        <AccordionItem title="Hello" content="World" />
+        <AccordionItem title="Hello" content="World" />
+        <AccordionItem title="Hello" content="World" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default About;
