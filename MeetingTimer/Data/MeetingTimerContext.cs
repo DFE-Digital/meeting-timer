b4e22afc75477400c5e9af7fc1116066ace82c1e
robyoung91 <robertmatthewyoung@gmail.com>
2021-03-05 16:43:39 +0000

    Amend MeetingTimerContext to remove sqlite

--- MeetingTimer/Data/MeetingTimerContext.cs
+++ MeetingTimer/Data/MeetingTimerContext.cs
@@ -5,11 +5,9 @@ namespace MeetingTimer.Data
 {
     public class MeetingTimerContext : DbContext
     {
+        public MeetingTimerContext(DbContextOptions<MeetingTimerContext> options) : base(options) { }
         public DbSet<Session> Sessions { get; set; }
         public DbSet<Host> Hosts { get; set; }
         public DbSet<Member> Members { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-          => optionsBuilder.UseSqlite("Data Source=Data/MeetingTimerDB.db");
     }
 }
