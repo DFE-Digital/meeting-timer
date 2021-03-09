e7d474c87c7fe4473dfe8f6a0bdc3665e137d4a0
robyoung91 <robertmatthewyoung@gmail.com>
2021-03-09 10:43:36 +0000

    Add initial migration

--- MeetingTimer/Data/Migrations/20210305162214_InitialMigration.Designer.cs
+++ MeetingTimer/Data/Migrations/20210305162214_InitialMigration.Designer.cs
@@ -2,15 +2,17 @@
 using MeetingTimer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace MeetingTimer.Data.Migrations
 {
     [DbContext(typeof(MeetingTimerContext))]
-    partial class MeetingTimerContextModelSnapshot : ModelSnapshot
+    [Migration("20210305162214_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
