96e48a8ede9e1ed4c25e55ecc8eb27ebacd9e580
Steven <steven.woods@digital.education.gov.uk>
2021-03-12 21:13:58 +0000

    try to fix connection string

--- MeetingTimer/Program.cs
+++ MeetingTimer/Program.cs
@@ -9,18 +9,22 @@ using System.Threading.Tasks;
 
 namespace MeetingTimer
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+ public class Program
+ {
+  public static void Main(string[] args)
+  {
+   CreateHostBuilder(args).Build().Run();
+  }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+  public static IHostBuilder CreateHostBuilder(string[] args) =>
+     Host.CreateDefaultBuilder(args)
+      .ConfigureAppConfiguration((hostingContext, config) =>
+      {
+       config.AddEnvironmentVariables();
+      })
+      .ConfigureWebHostDefaults(webBuilder =>
+      {
+       webBuilder.UseStartup<Startup>();
+      });
+ }
 }
--- MeetingTimer/Startup.cs
+++ MeetingTimer/Startup.cs
@@ -15,82 +15,81 @@ using System.Reflection;
 
 namespace MeetingTimer
 {
-    public class Startup
+ public class Startup
+ {
+  public Startup(IConfiguration configuration)
+  {
+   Configuration = configuration;
+  }
+
+  public IConfiguration Configuration { get; }
+
+  // This method gets called by the runtime. Use this method to add services to the container.
+  public void ConfigureServices(IServiceCollection services)
+  {
+
+   services.AddControllersWithViews();
+   services.AddSwaggerGen(c =>
+   {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MeetingTimer", Version = "v1" });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+   });
+
+   // In production, the React files will be served from this directory
+   services.AddSpaStaticFiles(configuration =>
+       configuration.RootPath = "ClientApp/build"
+   );
+
+   services.AddEntityFrameworkNpgsql().AddDbContext<MeetingTimerContext>(opt => opt.UseNpgsql(Configuration["PG_CONNECTION_STRING"]));
+
+   services.AddScoped<ISessionRepository, SessionRepository>();
+  }
+
+  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+  {
+   if (env.IsDevelopment())
+   {
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meetingtimer v1");
+     c.RoutePrefix = "api-documentation";
+    });
+   }
+   else
+   {
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+   }
 
-        public IConfiguration Configuration { get; }
+   app.UseHttpsRedirection();
+   app.UseStaticFiles();
+   app.UseSpaStaticFiles();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+   app.UseRouting();
 
-            services.AddControllersWithViews();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MeetingTimer", Version = "v1" });
+   app.UseEndpoints(endpoints =>
+   {
+    endpoints.MapControllerRoute(
+                 name: "default",
+                 pattern: "{controller}/{action=Index}/{id?}");
+   });
 
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
+   app.UseSpa(spa =>
+   {
+    spa.Options.SourcePath = "ClientApp";
 
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-                configuration.RootPath = "ClientApp/build"
-            );
-
-            var sqlConnectionString = Configuration["MeetingTimerConnectionString"];
-            services.AddEntityFrameworkNpgsql().AddDbContext<MeetingTimerContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("MeetingTimerConnectionString")));
-
-            services.AddScoped<ISessionRepository, SessionRepository>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meetingtimer v1");
-                    c.RoutePrefix = "api-documentation";
-                });
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
-            });
-        }
+    if (env.IsDevelopment())
+    {
+     spa.UseReactDevelopmentServer(npmScript: "start");
     }
+   });
+  }
+ }
 }
