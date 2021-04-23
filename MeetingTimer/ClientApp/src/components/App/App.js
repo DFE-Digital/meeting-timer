af262dc8bc61e1904bbb71fee32f20906abf6ee3
FKANE <fg.kane@gmail.com>
2021-04-23 13:53:46 +0100

    Added Countdown class component

--- MeetingTimer/ClientApp/src/components/App/App.js
+++ MeetingTimer/ClientApp/src/components/App/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router';
 import Layout from '../Layout';
 import Home from '../../pages/Home';
 import About from '../../pages/About';
+import Countdown from '../Countdown';
 
 const App = () => (
   <Layout>
     <Route exact path="/" component={Home} />
     <Route exact path="/about" component={About} />
+    <Countdown/>
   </Layout>
 );
 
